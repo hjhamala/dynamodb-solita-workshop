d9dd2d350e0513dc6cb44824c43832a81481596c
Heikki Hämäläinen <heikki.hamalainen@solita.fi>
2021-10-19 17:31:23 +0300

    Add search item and use getItem twice to search user

--- src/db.ts
+++ src/db.ts
@@ -1,36 +1,52 @@
-import {config, DynamoDB} from 'aws-sdk';
-import {v4 as uuid} from 'uuid';
+import { config, DynamoDB } from 'aws-sdk';
+import { v4 as uuid } from 'uuid';
 
-const tableName = 'Test'
+const tableName = 'Test';
 
-config.update({region: 'eu-west-1'});
+config.update({ region: 'eu-west-1' });
 
 export const dynamo = new DynamoDB.DocumentClient({
-    apiVersion: '2012-08-10',
-    endpoint: 'http://localhost:8000',
+  apiVersion: '2012-08-10',
+  endpoint: 'http://localhost:8000',
 });
 
 export async function addUser(name: string, email: string): Promise<string> {
-    const userId = uuid()
-    await dynamo.put({TableName: tableName, Item: {pk: 'user', sk: userId, name, email}}).promise()
-    return userId
+  const userId = uuid();
+  const userItem = { pk: userId, sk: 'user', name, email };
+  const searchItem = { pk: 'email', sk: email, userId };
+
+  await dynamo
+    .transactWrite({
+      TransactItems: [
+        { Put: { TableName: tableName, Item: userItem } },
+        { Put: { TableName: tableName, Item: searchItem } },
+      ],
+    })
+    .promise();
+  return userId;
 }
 
-export async function searchUser(emailToken: string): Promise<{ name: string, email: string, id: string } | undefined> {
-    const params = {
-        TableName: tableName,
-        FilterExpression: '#email = :emailToken',
-        ExpressionAttributeNames: {"#email": 'email'},
-        ExpressionAttributeValues: {":emailToken": emailToken}
-    }
+export async function searchUser(
+  emailToken: string
+): Promise<{ name: string; email: string; id: string } | undefined> {
+  const params = {
+    TableName: tableName,
+    Key: { pk: 'email', sk: emailToken },
+  };
 
-    const result = await dynamo.scan(params).promise()
+  const result = await dynamo.get(params).promise();
 
-    if (result.Items.length === 0) {
-        return undefined
-    }
+  if (typeof result.Item === 'undefined') {
+    return undefined;
+  }
 
-    const {email, name, id} = result.Items[0]
+  const userParams = {
+    TableName: tableName,
+    Key: { pk: result.Item.userId, sk: 'user' },
+  };
+  const user = await dynamo.get(userParams).promise();
 
-    return {email, name, id}
+  const { email, name, id } = user.Item;
+
+  return { email, name, id };
 }
