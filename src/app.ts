bc97752ee0c3c73fa852edcb8f4a75492c0d5617
Heikki Hämäläinen <heikki.hamalainen@solita.fi>
2021-10-20 08:55:31 +0300

    Topic can be added and user has list of topics

--- src/app.ts
+++ src/app.ts
@@ -1,61 +1,79 @@
 import * as express from 'express';
-import * as bodyParser from 'body-parser'
-import {NextFunction, RequestHandler, Request, Response} from 'express';
-import {addUser, searchUser} from "./db";
+import * as bodyParser from 'body-parser';
+import { NextFunction, RequestHandler, Request, Response } from 'express';
+import { addTopic, addUser, getTopicsByUser, searchUser } from './db';
 
 const wrapAsyncHandlerError = (handler: RequestHandler) => (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<void> => {
-    return Promise.resolve(handler(req, res, next)).catch(error => {
-        console.error('Error in API call', {
-            path: req.path,
-            error,
-            method: req.method,
-            params: req.params,
-            query: req.query,
-        })
+  return Promise.resolve(handler(req, res, next)).catch((error) => {
+    console.error('Error in API call', {
+      path: req.path,
+      error,
+      method: req.method,
+      params: req.params,
+      query: req.query,
+    });
 
-        res.set('Content-Type', 'application/json')
-        res.status(500).send({})
-    })
-}
+    res.set('Content-Type', 'application/json');
+    res.status(500).send({});
+  });
+};
 
 export const app = express();
 app.use(express.json());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.get(
-    '/health',
-    wrapAsyncHandlerError(async (req: Request, res) => {
-        res.send({message: 'ok'})
-    })
-)
+  '/health',
+  wrapAsyncHandlerError(async (req: Request, res) => {
+    res.send({ message: 'ok' });
+  })
+);
 
 app.post(
-    '/user',
-    wrapAsyncHandlerError(async (req: Request, res) => {
-        const {name, email} = req.body
-        const id = await addUser(name, email)
+  '/user',
+  wrapAsyncHandlerError(async (req: Request, res) => {
+    const { name, email } = req.body;
+    const id = await addUser(name, email);
 
-        res.send({name, email, id})
-    })
-)
+    res.send({ name, email, id });
+  })
+);
 
 app.get(
-    '/user/search/:email',
-    wrapAsyncHandlerError(async (req: Request, res) => {
-        const searchToken = req.params.email
+  '/user/:userId/topic',
+  wrapAsyncHandlerError(async (req: Request, res) => {
+    const userId = req.params.userId;
+    const topics = await getTopicsByUser(userId);
 
-        const searchResult = await searchUser(searchToken)
+    res.send({ topics });
+  })
+);
 
-        if (typeof searchResult === 'undefined'){
-            return res.status(404).send()
-        }
+app.get(
+  '/user/search/:email',
+  wrapAsyncHandlerError(async (req: Request, res) => {
+    const searchToken = req.params.email;
 
-        res.send(searchResult)
-    })
-)
+    const searchResult = await searchUser(searchToken);
 
+    if (typeof searchResult === 'undefined') {
+      return res.status(404).send();
+    }
 
+    res.send(searchResult);
+  })
+);
+
+app.post(
+  '/topic',
+  wrapAsyncHandlerError(async (req: Request, res) => {
+    const { userId, topicName } = req.body;
+    const topicId = await addTopic(userId, topicName);
+
+    res.send({ topicId });
+  })
+);
