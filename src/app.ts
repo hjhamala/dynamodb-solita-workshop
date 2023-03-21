c83497826cb22c0c18153552ab4b98e7768fd8cd
Heikki Hämäläinen <heikki.hamalainen@solita.fi>
2023-03-21 14:22:59 +0200

    Return id correctly in search

--- src/app.ts
+++ src/app.ts
@@ -1,61 +1,67 @@
-import * as express from 'express';
+import * as express from 'express'
 import * as bodyParser from 'body-parser'
-import {NextFunction, RequestHandler, Request, Response} from 'express';
-import {addUser, searchUser} from "./db";
+import { NextFunction, RequestHandler, Request, Response } from 'express'
+import { addUser, searchUser } from './db'
 
 const wrapAsyncHandlerError = (handler: RequestHandler) => (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ): Promise<void> => {
-    return Promise.resolve(handler(req, res, next)).catch(error => {
-        console.error('Error in API call', {
-            path: req.path,
-            error,
-            method: req.method,
-            params: req.params,
-            query: req.query,
-        })
-
-        res.set('Content-Type', 'application/json')
-        res.status(500).send({})
+  return Promise.resolve(handler(req, res, next)).catch((error) => {
+    console.error('Error in API call', {
+      path: req.path,
+      error,
+      method: req.method,
+      params: req.params,
+      query: req.query,
     })
+
+    res.set('Content-Type', 'application/json')
+    res.status(500).send({})
+  })
 }
 
-export const app = express();
-app.use(express.json());
+export const app = express()
+app.use(express.json())
 app.use(bodyParser.json())
 
 app.get(
-    '/health',
-    wrapAsyncHandlerError(async (req: Request, res) => {
-        res.send({message: 'ok'})
-    })
+  '/health',
+  wrapAsyncHandlerError(async (req: Request, res) => {
+    res.send({ message: 'ok' })
+  })
 )
 
 app.post(
-    '/user',
-    wrapAsyncHandlerError(async (req: Request, res) => {
-        const {name, email} = req.body
-        const id = await addUser(name, email)
+  '/user',
+  wrapAsyncHandlerError(async (req: Request, res) => {
+    const { name, email } = req.body
 
-        res.send({name, email, id})
-    })
+    if (!name || !email) {
+      return res.status(400)
+    }
+
+    const id = await addUser(name, email)
+
+    res.send({ name, email, id })
+  })
 )
 
 app.get(
-    '/user/search/:email',
-    wrapAsyncHandlerError(async (req: Request, res) => {
-        const searchToken = req.params.email
+  '/user/search/:email',
+  wrapAsyncHandlerError(async (req: Request, res) => {
+    const searchToken = req.params.email
 
-        const searchResult = await searchUser(searchToken)
+    if (!searchToken) {
+      return res.send(400)
+    }
+    const searchResult = await searchUser(searchToken)
 
-        if (typeof searchResult === 'undefined'){
-            return res.status(404).send()
-        }
+    if (typeof searchResult === 'undefined') {
+      return res.status(404).send()
+    }
 
-        res.send(searchResult)
-    })
+    res.send(searchResult)
+  })
 )
-
-
